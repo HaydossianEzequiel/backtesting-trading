edfc1a0827b965530d1b6d69b17be6439d76a684
Ezequiel Haydossian <ezeqhay@hotmail.com>
2019-11-24 22:18:09 -0300

    levanto el html de un archivo

--- src/main/java/frontend/FrontendSource.java
+++ src/main/java/frontend/FrontendSource.java
@@ -1,33 +1,23 @@
 package frontend;
 
-import api.model.DataStock;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class FrontendSource {
 
-    public List<DataStock> getHtml(String stockName) {
+    public String get() {
         try{
             String fullName = new File("").getAbsolutePath() + "/src/main/java/frontend/" + "ejemplo" + ".html";
             BufferedReader br = new BufferedReader(new FileReader(fullName));
             String line = null;
-            List<DataStock> dataStocks = new ArrayList<>();
-
+            String page = "";
             while ((line = br.readLine()) != null) {
-                String str[] = line.split(", ");
-                if (!str[0].equals("Date")) {
-                    String date = str[0].replace(".", ",");
-                    String close = str[1].replace(".", ",");
-                    dataStocks.add(new DataStock(date, close));
-                }
+                page = page + line;
 
             }
-            return dataStocks;
+            return page;
         } catch(IOException e) {
             throw new RuntimeException(e);
         }
